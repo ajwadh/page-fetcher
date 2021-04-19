dfa1300c75edfc0283c2a8fce97dd457c198c04d
Ajwad Hossain <hossain.ajwad@gmail.com>
2021-04-19 01:23:32 +0000

    complete

---
+++ fetcher.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const request = require("request");
+const readline = require('readline');
+
+const url = process.argv[2]
+const path = process.argv[3];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const fetcher = function (url, path) {
+  request(url, (error, response, body) => {
+    fs.exists(path, () => {
+      rl.question('The file already exist. Do you wish to overwrite it? Y:', (answer) => {
+        if (answer === 'y') {
+          fs.writeFile(path, body, () => {
+            const bytes = response.headers['content-length'];
+            console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
+            process.exit()
+            return;
+          })
+        } else {
+          process.exit()
+        }
+        rl.close();
+      })
+    })
+  })
+}
+
+fetcher(url, path)
